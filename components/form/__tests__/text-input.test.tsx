51e8cd5dd60a2d4bd803860b2566e72d682ffc1e
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-10-27 09:42:31 -0700

    Added tests to text-input

--- components/form/__tests__/text-input.test.tsx
+++ components/form/__tests__/text-input.test.tsx
@@ -1,16 +1,491 @@
 import { shallow } from 'enzyme'
 
+import { User } from '../../../elements/icon'
+import type { Autocomplete } from '../helpers/text-input-base'
 import { TextInput } from '../text-input'
 
 test('renders without crashing', () => {
-    const wrapper = shallow(<TextInput type='text' name='text input' />)
+    const wrapper = shallow(<TextInput type='text' name='text-input' />)
     expect(wrapper.isEmptyRender()).toEqual(false)
 })
 
-// all the visual stuff
+/* 
+   
+   
+   
+   all the visual stuff
+   
+   
+   
+    */
 
-// that the unnconfigured input formats functions properly (e.g., onChange updates input, blur validates, etc.)
+describe('make sure either label, id, or name is defined', () => {
+    test('throw error if all are undefined', () => {
+        expect(() => {
+            shallow(<TextInput type='text' />)
+        }).toThrow()
+    })
+})
 
-// test hooks for validation, formatting, etc.
+describe('"label" prop is passed down', () => {
+    const labelValues = ['Name', 'Email', 'Username', 'The second month that ever happened']
+    test.each(labelValues)('with a label of "%s"', (label) => {
+        const wrapper = shallow(<TextInput type='text' label={label} />).find('TextInputBase')
+        expect(wrapper.prop('label')).toBe(label)
+        expect(wrapper.prop('name')).toBeDefined()
+        expect(wrapper.prop('id')).toBeDefined()
+    })
+    test('label is "" when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('label')
+        ).toBe('')
+    })
+})
 
-// test interaction with formSync
+describe('"name" prop is passed down', () => {
+    const nameValues = ['name', 'email', 'username', 'the-third-month-that-ever-happened']
+    test.each(nameValues)('with an name of "%s"', (name) => {
+        const wrapper = shallow(<TextInput type='text' name={name} />).find('TextInputBase')
+        expect(wrapper.prop('name')).toBe(name)
+    })
+    test.each(nameValues)('name is derived from label when not defined', (label) => {
+        expect(
+            shallow(<TextInput type='url' label={label} />)
+                .find('TextInputBase')
+                .prop('name')
+        ).toBeDefined()
+    })
+})
+
+describe('"id" prop is passed down', () => {
+    const idValues = ['name', 'email', 'username', 'the-third-month-that-ever-happened']
+    test.each(idValues)('with an id of "%s"', (id) => {
+        const wrapper = shallow(<TextInput type='text' id={id} />).find('TextInputBase')
+        expect(wrapper.prop('id')).toBe(id)
+    })
+    test.each(idValues)('id is name when not defined', (name) => {
+        expect(
+            shallow(<TextInput type='url' name={name} />)
+                .find('TextInputBase')
+                .prop('id')
+        ).toBe(name)
+    })
+})
+
+describe('"type" prop is passed down', () => {
+    test.each(['text', 'email', 'number', 'password', 'search', 'textarea', 'url'])(
+        '%s type',
+        (type) => {
+            expect(
+                shallow(
+                    <TextInput
+                        type={
+                            type as
+                                | 'text'
+                                | 'email'
+                                | 'number'
+                                | 'password'
+                                | 'search'
+                                | 'textarea'
+                                | 'url'
+                        }
+                        name='text-input'
+                    />
+                )
+                    .find('TextInputBase')
+                    .prop('type')
+            ).toBe(type)
+        }
+    )
+})
+
+describe('"placeholder" prop is passed down', () => {
+    test.each(['', 'Hello world', 'defaultValue', 'Default value', '$1,000.00'])(
+        'with a placeholder of "%s"',
+        (placeholder) => {
+            expect(
+                shallow(<TextInput type='text' name='text-input' placeholder={placeholder} />)
+                    .find('TextInputBase')
+                    .prop('placeholder')
+            ).toBe(placeholder)
+        }
+    )
+    test('placeholder is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('placeholder')
+        ).toBeUndefined()
+    })
+})
+
+describe('"defaultValue" prop is passed down', () => {
+    test.each(['', 'Hello world', 'defaultValue', 'Default value', '$1,000.00'])(
+        'with a placeholder of "%s"',
+        (value) => {
+            expect(
+                shallow(<TextInput type='text' name='text-input' defaultValue={value} />)
+                    .find('TextInputBase')
+                    .prop('value')
+            ).toBe(value)
+        }
+    )
+    test('value is an empty string when defaultValue is undefined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('value')
+        ).toBe('')
+    })
+})
+
+describe('"size" prop is passed down', () => {
+    test.each(['small', 'default', 'large'])('with a size "%s"', (size) => {
+        expect(
+            shallow(
+                <TextInput
+                    type='text'
+                    name='text-input'
+                    size={size as 'small' | 'default' | 'large'}
+                />
+            )
+                .find('TextInputBase')
+                .prop('size')
+        ).toBe(size)
+    })
+    test('size is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('size')
+        ).toBeUndefined()
+    })
+})
+
+describe('"optional" prop is passed down', () => {
+    test.each([true, false])('optional is "%p"', (optional) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' optional={optional} />)
+                .find('TextInputBase')
+                .prop('optional')
+        ).toBe(optional)
+    })
+    test('optional is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('optional')
+        ).toBeUndefined()
+    })
+})
+
+describe('"hideOptionalLabel" prop is passed down', () => {
+    test.each([true, false, undefined])('optional is "%p"', (value) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' hideOptionalLabel={value} />)
+                .find('TextInputBase')
+                .prop('hideOptionalLabel')
+        ).toBe(value)
+    })
+    test('hideOptionalLabel is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('hideOptionalLabel')
+        ).toBeUndefined()
+    })
+})
+
+describe('"readonly" prop is passed down', () => {
+    test.each([true, false])('optional is "%p"', (readonly) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' readonly={readonly} />)
+                .find('TextInputBase')
+                .prop('readonly')
+        ).toBe(readonly)
+    })
+    test('readonly is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('readonly')
+        ).toBeUndefined()
+    })
+})
+
+describe('"disabled" prop is passed down', () => {
+    test.each([true, false])('disabled is "%p"', (disabled) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' disabled={disabled} />)
+                .find('TextInputBase')
+                .prop('disabled')
+        ).toBe(disabled)
+    })
+    test('disabled is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('disabled')
+        ).toBeUndefined()
+    })
+})
+
+describe('"textRight" prop is passed down', () => {
+    test.each([true, false])('textRight is "%p"', (textRight) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' textRight={textRight} />)
+                .find('TextInputBase')
+                .prop('textRight')
+        ).toBe(textRight)
+    })
+    test('textRight is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('textRight')
+        ).toBeUndefined()
+    })
+})
+
+describe('"tabNums" prop is passed down', () => {
+    test.each([true, false])('tabNums is "%p"', (tabNums) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' tabNums={tabNums} />)
+                .find('TextInputBase')
+                .prop('tabNums')
+        ).toBe(tabNums)
+    })
+    test('tabNums is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('tabNums')
+        ).toBeUndefined()
+    })
+})
+
+describe('"help" prop is passed down', () => {
+    test.each([
+        '',
+        'This is some help text',
+        'We never sell or share your email to anyone',
+        '(###) ### - ####',
+    ])('help is "%p"', (help) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' help={help} />)
+                .find('TextInputBase')
+                .prop('help')
+        ).toBe(help)
+    })
+    test('help is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('help')
+        ).toBeUndefined()
+    })
+})
+
+describe('"info" prop is passed down', () => {
+    test.each([
+        '',
+        'This is some info text',
+        'We never sell or share your email to anyone',
+        '(###) ### - ####',
+    ])('info is "%p"', (info) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' info={info} />)
+                .find('TextInputBase')
+                .prop('info')
+        ).toBe(info)
+    })
+    test('info is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('info')
+        ).toBeUndefined()
+    })
+})
+
+describe('"keyboard" prop is passed down', () => {
+    test.each(['email', 'search', 'url', 'decimal', 'none', 'numeric', 'tel'])(
+        'keyboard is "%p"',
+        (keyboard) => {
+            expect(
+                shallow(
+                    <TextInput
+                        type='text'
+                        name='text-input'
+                        keyboard={
+                            keyboard as
+                                | 'email'
+                                | 'search'
+                                | 'url'
+                                | 'decimal'
+                                | 'none'
+                                | 'numeric'
+                                | 'tel'
+                        }
+                    />
+                )
+                    .find('TextInputBase')
+                    .prop('keyboard')
+            ).toBe(keyboard)
+        }
+    )
+    test('keyboard is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('keyboard')
+        ).toBeUndefined()
+    })
+})
+
+describe('"autoComplete" prop is passed down', () => {
+    // eslint-disable-next-line prettier/prettier -- will be horrendous if each value is on its own line
+    test.each(['', 'off', 'on', 'name', 'honorific-prefix', 'given-name', 'additional-name', 'family-name', 'honorific-suffix', 'nickname', 'email', 'username', 'new-password', 'current-password', 'one-time-code', 'organization-title', 'organization', 'street-address', 'address-line1', 'address-line2', 'address-line3', 'address-level1', 'address-level2', 'address-level3', 'address-level4', 'country', 'country-name', 'postal-code', 'cc-name', 'cc-given-name', 'cc-additional-name', 'cc-family-name', 'cc-number', 'cc-exp', 'cc-exp-month', 'cc-exp-year', 'cc-csc', 'cc-type', 'transaction-currency', 'transaction-amount', 'language', 'bday', 'bday-day', 'bday-month', 'bday-year', 'sex', 'tel', 'tel-country-code', 'tel-national', 'tel-area-code', 'tel-local', 'tel-extension', 'impp', 'url', 'photo',
+    ])('autoComplete is "%p"', (autoComplete) => {
+        expect(
+            shallow(
+                <TextInput
+                    type='text'
+                    name='text-input'
+                    autoComplete={autoComplete as Autocomplete}
+                />
+            )
+                .find('TextInputBase')
+                .prop('autoComplete')
+        ).toBe(autoComplete)
+    })
+    test('autoComplete is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('autoComplete')
+        ).toBeUndefined()
+    })
+})
+
+describe('"prefix" prop is passed down', () => {
+    test.each(['', '$', '¥', <User key='user' />])('prefix is "%p"', (prefix) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' prefix={prefix} />)
+                .find('TextInputBase')
+                .prop('prefix')
+        ).toBe(prefix)
+    })
+    test('prefix is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('prefix')
+        ).toBeUndefined()
+    })
+})
+
+describe('"suffix" prop is passed down', () => {
+    test.each(['', '$', '¥', <User key='user' />])('suffix is "%p"', (suffix) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' suffix={suffix} />)
+                .find('TextInputBase')
+                .prop('suffix')
+        ).toBe(suffix)
+    })
+    test('suffix is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('suffix')
+        ).toBeUndefined()
+    })
+})
+
+describe('"prefixOnClick" prop is passed down', () => {
+    test.each([undefined, jest.fn])('suffix is "%p"', (prefixOnClick) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' prefixOnClick={prefixOnClick} />)
+                .find('TextInputBase')
+                .prop('prefixOnClick')
+        ).toBe(prefixOnClick)
+    })
+    test('prefixOnClick is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('prefixOnClick')
+        ).toBeUndefined()
+    })
+})
+
+describe('"suffixOnClick" prop is passed down', () => {
+    test.each([undefined, jest.fn])('suffix is "%p"', (suffixOnClick) => {
+        expect(
+            shallow(<TextInput type='text' name='text-input' suffixOnClick={suffixOnClick} />)
+                .find('TextInputBase')
+                .prop('suffixOnClick')
+        ).toBe(suffixOnClick)
+    })
+    test('suffixOnClick is undefined when not defined', () => {
+        expect(
+            shallow(<TextInput type='url' name='text-input' />)
+                .find('TextInputBase')
+                .prop('suffixOnClick')
+        ).toBeUndefined()
+    })
+})
+
+/* 
+   
+   
+   
+   that the unnconfigured input formats functions properly (e.g., onChange updates input, blur validates, etc.)
+   
+   
+   
+    */
+
+describe("typing into the input updates the input's value", () => {
+    it.todo('as a user types, the value shown in the input updates')
+})
+
+/* const input = mount(<TextInput type='text' label='text-input' />).find('input')
+   expect(input.prop('value')).toBe('')
+   act(() => {
+       input.prop('onChange')({ currentTarget: { value: 'Hello' } })
+   })
+   expect(input.prop('value')).toBe('Hello') */
+
+describe('input is invalid when input is empty and field is required', () => {
+    test.todo('marked as invalid only onBlur')
+    test.todo('default required message is shown')
+    test.todo('custom required message is shown')
+})
+
+describe('success message is shown when field is valid', () => {
+    test.todo('custom success message is shown')
+    test.todo('success message is not shown if not provided')
+})
+
+/* 
+   
+   
+   
+   test hooks for validation, formatting, etc.
+   
+   
+   
+    */
+
+/* 
+   
+   
+   
+   test interaction with formSync
+   
+   
+   
+    */
