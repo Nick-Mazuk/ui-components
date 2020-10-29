2e6c15a26bde173422f3b001362ff54595edbe3c
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-10-29 14:47:31 -0700

    realtime tests upgraded to testing-library-react

--- elements/__tests__/real-time.test.tsx
+++ elements/__tests__/real-time.test.tsx
@@ -1,34 +1,13 @@
-import { shallow } from 'enzyme'
+import { render } from '@testing-library/react'
 
 import { RealTime } from '../real-time'
 
 test('renders without crashing', () => {
-    const wrapper = shallow(<RealTime />)
-    expect(wrapper.isEmptyRender()).toEqual(false)
+    const { baseElement } = render(<RealTime />)
+    expect(baseElement).not.toBeEmptyDOMElement()
 })
 
-test('shows default label', () => {
-    expect(
-        shallow(<RealTime />)
-            .find('Text')
-            .prop('children')
-    ).toEqual('Updating live')
-})
-
-test('shows custom label', () => {
-    expect(
-        shallow(<RealTime label='Live stats' />)
-            .find('Text')
-            .prop('children')
-    ).toEqual('Live stats')
-    expect(
-        shallow(<RealTime label='Breaking news' />)
-            .find('Text')
-            .prop('children')
-    ).toEqual('Breaking news')
-    expect(
-        shallow(<RealTime label='Breaking news.' />)
-            .find('Text')
-            .prop('children')
-    ).toEqual('Breaking news.')
+const labels = [undefined, 'Live stats', 'Breaking news', 'Breaking news.']
+test.each(labels)('shows label "%s"', (label) => {
+    render(<RealTime label={label} />)
 })
