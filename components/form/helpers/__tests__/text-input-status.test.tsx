1d4feffc5430afc476939c7b960ade51966b08a8
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-10-29 11:58:59 -0700

    text-input-status tests upgraded to react-testing-library

--- components/form/helpers/__tests__/text-input-status.test.tsx
+++ components/form/helpers/__tests__/text-input-status.test.tsx
@@ -1,38 +1,31 @@
-import { shallow } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 
 import { Status } from '../text-input-status'
 
 test('renders without crashing', () => {
-    const wrapper = shallow(<Status readonly optional />)
-    expect(wrapper.isEmptyRender()).toEqual(false)
+    const { baseElement } = render(<Status readonly optional />)
+    expect(baseElement).not.toBeEmptyDOMElement()
+})
+
+test("is empty when everything's false", () => {
+    const { container } = render(<Status readonly={false} optional={false} />)
+    expect(container).toBeEmptyDOMElement()
 })
 
 describe('shows correct state', () => {
     test('readonly displayed when readonly', () => {
-        expect(
-            shallow(<Status readonly optional={false} />)
-                .find('Text')
-                .prop('children')
-        ).toContain('readonly')
-        expect(
-            shallow(<Status readonly optional />)
-                .find('Text')
-                .prop('children')
-        ).toContain('readonly')
+        render(<Status readonly optional={false} />)
+        expect(screen.getByText(/readonly/u)).toBeTruthy()
+        expect(screen.queryByText(/optional/u)).toBeFalsy()
+    })
+    test('readonly displayed when readonly and optional', () => {
+        render(<Status readonly optional />)
+        expect(screen.getByText(/readonly/u)).toBeTruthy()
+        expect(screen.queryByText(/optional/u)).toBeFalsy()
     })
     test('optional displayed when optional', () => {
-        expect(
-            shallow(<Status readonly={false} optional />)
-                .find('Text')
-                .prop('children')
-        ).toContain('optional')
+        render(<Status readonly={false} optional />)
+        expect(screen.queryByText(/readonly/u)).toBeFalsy()
+        expect(screen.getByText(/optional/u)).toBeTruthy()
     })
-})
-
-test('is empty when neither readonly or optional', () => {
-    expect(
-        shallow(<Status readonly={false} optional={false} />)
-            .children()
-            .exists()
-    ).toEqual(false)
 })
