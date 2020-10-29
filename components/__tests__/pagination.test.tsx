01e62cbe7c7511bfe100190b476c73bf777aa28b
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-10-29 15:02:51 -0700

    pagination tests upgraded to testing-library-react

--- components/__tests__/pagination.test.tsx
+++ components/__tests__/pagination.test.tsx
@@ -1,4 +1,4 @@
-import { mount, render, shallow } from 'enzyme'
+import { render, screen } from '@testing-library/react'
 
 import { Pagination } from '../pagination'
 
@@ -11,74 +11,49 @@ import { Pagination } from '../pagination'
 const handlePageChange = jest.fn()
 
 test('renders without crashing', () => {
-    expect(
-        shallow(
-            <Pagination current={3} pageCount={10} onPageChange={handlePageChange} />
-        ).isEmptyRender()
-    ).toEqual(false)
+    const { baseElement } = render(
+        <Pagination current={3} pageCount={10} onPageChange={handlePageChange} />
+    )
+    expect(baseElement).not.toBeEmptyDOMElement()
 })
 
-describe('current button is always highlighted', () => {
-    test("if current is the first button, it's still blue", () => {
-        const button = mount(
-            <Pagination current={1} pageCount={10} onPageChange={handlePageChange} />
+const pageCombos = [
+    [1, 10],
+    [4, 10],
+    [4, 4],
+]
+test.each(pageCombos)(
+    'only the current button is highlighted with current: "%s", pageCount: "%s"',
+    (current, pageCount) => {
+        const { container } = render(
+            <Pagination current={current} pageCount={pageCount} onPageChange={handlePageChange} />
         )
-            .find('Button')
-            .findWhere((element) => element.prop('value') === '1')
-        expect(button.prop('color')).toEqual('primary')
-    })
-    test("if current is in the middle, it's still blue", () => {
-        const button = mount(
-            <Pagination current={4} pageCount={10} onPageChange={handlePageChange} />
-        )
-            .find('Button')
-            .findWhere((element) => element.prop('value') === '4')
-        expect(button.prop('color')).toEqual('primary')
-    })
-    test("if current is last, it's still blue", () => {
-        const button = mount(
-            <Pagination current={4} pageCount={4} onPageChange={handlePageChange} />
-        )
-            .find('Button')
-            .findWhere((element) => element.prop('value') === '4')
-        expect(button.prop('color')).toEqual('primary')
-    })
-})
+        const buttons = container.querySelectorAll('button.bg-primary')
+        expect(buttons).toHaveLength(1)
+        expect(buttons[0]).toHaveTextContent(String(current))
+    }
+)
 
 describe('next/previous buttons are shown', () => {
     test('previous button is not shown when current page is 1', () => {
-        expect(
-            render(<Pagination current={1} pageCount={10} onPageChange={handlePageChange} />)
-                .html()
-                ?.match('Previous')
-        ).toBeNull()
+        render(<Pagination current={1} pageCount={10} onPageChange={handlePageChange} />)
+        expect(screen.queryByText('Previous')).toBeFalsy()
     })
     test('previous button is shown when current page is not 1', () => {
-        expect(
-            render(<Pagination current={2} pageCount={10} onPageChange={handlePageChange} />)
-                .html()
-                ?.match('Previous')
-        ).toBeTruthy()
+        render(<Pagination current={2} pageCount={10} onPageChange={handlePageChange} />)
+        expect(screen.getByText('Previous')).toBeTruthy()
     })
     test('next button is not shown when current page is same as pageCount', () => {
-        expect(
-            render(<Pagination current={10} pageCount={10} onPageChange={handlePageChange} />)
-                .html()
-                ?.match('Next')
-        ).toBeNull()
+        render(<Pagination current={10} pageCount={10} onPageChange={handlePageChange} />)
+        expect(screen.queryByText('Next')).toBeFalsy()
     })
     test('next button is shown when current page is less than pageCount', () => {
-        expect(
-            render(<Pagination current={3} pageCount={10} onPageChange={handlePageChange} />)
-                .html()
-                ?.match('Next')
-        ).toBeTruthy()
+        render(<Pagination current={2} pageCount={10} onPageChange={handlePageChange} />)
+        expect(screen.getByText('Next')).toBeTruthy()
     })
     test('if pageCount equals one, neither button is shown', () => {
-        const wrapper = render(
-            <Pagination current={1} pageCount={1} onPageChange={handlePageChange} />
-        ).html()
-        expect(wrapper?.match('Previous')).toBeNull()
-        expect(wrapper?.match('Next')).toBeNull()
+        render(<Pagination current={1} pageCount={1} onPageChange={handlePageChange} />)
+        expect(screen.queryByText('Previous')).toBeFalsy()
+        expect(screen.queryByText('Next')).toBeFalsy()
     })
 })
