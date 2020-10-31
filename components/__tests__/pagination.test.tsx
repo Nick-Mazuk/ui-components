8c2112388cfcb8483c0a75978d47b7a3d8a9a681
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-10-30 20:00:45 -0700

    Pagination tests complete

--- components/__tests__/pagination.test.tsx
+++ components/__tests__/pagination.test.tsx
@@ -1,12 +1,7 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Pagination } from '../pagination'
-
-/* left:
-   - test that pressing each button calls the onPageChange function
-   - test that each function call has the correct parameters
-   - ensure that there are enough surrounding buttons
-   - ensure that separators are present when necessary */
 
 const handlePageChange = jest.fn()
 
@@ -17,11 +12,60 @@ test('renders without crashing', () => {
     expect(baseElement).not.toBeEmptyDOMElement()
 })
 
-const pageCombos = [
-    [1, 10],
-    [4, 10],
-    [4, 4],
+// current, pageCount, surroundCurrent = 1, expectedButtons, number of separators
+type PageCombo = [number, number, number | undefined, (number | 'Previous' | 'Next')[], 0 | 1 | 2]
+
+/* eslint-disable no-magic-numbers -- defining the magic numbers*/
+const pageCombos: PageCombo[] = [
+    [1, 1, undefined, [1], 0],
+    [1, 10, undefined, [1, 2, 10, 'Next'], 1],
+    [4, 10, undefined, ['Previous', 1, 3, 4, 5, 10, 'Next'], 2],
+    [4, 4, undefined, ['Previous', 1, 3, 4], 1],
+    [2, 3, undefined, ['Previous', 1, 2, 3, 'Next'], 0],
+    [1, 11, 2, [1, 2, 3, 11, 'Next'], 1],
+    [4, 11, 2, ['Previous', 1, 2, 3, 4, 5, 6, 11, 'Next'], 1],
+    [5, 5, 2, ['Previous', 1, 3, 4, 5], 1],
 ]
+/* eslint-enable no-magic-numbers -- defining the magic numbers*/
+
+test.each(pageCombos)(
+    'correct buttons are shown. current: "%s", pageCount: "%s", surroundCurrent: "%s"',
+    (current, pageCount, surroundCurrent, shownButtons) => {
+        render(
+            <Pagination
+                current={current}
+                pageCount={pageCount}
+                surroundCurrent={surroundCurrent}
+                onPageChange={handlePageChange}
+            />
+        )
+        const buttons = screen.getAllByRole('button')
+        expect(buttons).toHaveLength(shownButtons.length)
+        for (const button of buttons) {
+            const content = button.textContent
+            if (content === 'Previous' || content === 'Next')
+                expect(shownButtons.includes(content)).toBeTruthy()
+            else if (content) expect(shownButtons.includes(parseInt(content))).toBeTruthy()
+            else expect(false).toBeTruthy()
+        }
+    }
+)
+
+test.each(pageCombos)(
+    'correct separators are shown. current: "%s", pageCount: "%s", surroundCurrent: "%s"',
+    (current, pageCount, surroundCurrent, _, separatorCount) => {
+        render(
+            <Pagination
+                current={current}
+                pageCount={pageCount}
+                surroundCurrent={surroundCurrent}
+                onPageChange={handlePageChange}
+            />
+        )
+        expect(screen.queryAllByText('…')).toHaveLength(separatorCount)
+    }
+)
+
 test.each(pageCombos)(
     'only the current button is highlighted with current: "%s", pageCount: "%s"',
     (current, pageCount) => {
@@ -34,26 +78,21 @@ test.each(pageCombos)(
     }
 )
 
-describe('next/previous buttons are shown', () => {
-    test('previous button is not shown when current page is 1', () => {
-        render(<Pagination current={1} pageCount={10} onPageChange={handlePageChange} />)
-        expect(screen.queryByText('Previous')).toBeFalsy()
-    })
-    test('previous button is shown when current page is not 1', () => {
-        render(<Pagination current={2} pageCount={10} onPageChange={handlePageChange} />)
-        expect(screen.getByText('Previous')).toBeTruthy()
-    })
-    test('next button is not shown when current page is same as pageCount', () => {
-        render(<Pagination current={10} pageCount={10} onPageChange={handlePageChange} />)
-        expect(screen.queryByText('Next')).toBeFalsy()
-    })
-    test('next button is shown when current page is less than pageCount', () => {
-        render(<Pagination current={2} pageCount={10} onPageChange={handlePageChange} />)
-        expect(screen.getByText('Next')).toBeTruthy()
-    })
-    test('if pageCount equals one, neither button is shown', () => {
-        render(<Pagination current={1} pageCount={1} onPageChange={handlePageChange} />)
-        expect(screen.queryByText('Previous')).toBeFalsy()
-        expect(screen.queryByText('Next')).toBeFalsy()
-    })
-})
+test.each(pageCombos)(
+    'all buttons call onPageChange with correct parameters: "%s", pageCount: "%s"',
+    (current, pageCount) => {
+        const mock = jest.fn()
+        render(<Pagination current={current} pageCount={pageCount} onPageChange={mock} />)
+        const buttons = screen.getAllByRole('button')
+
+        for (const [index, button] of buttons.entries()) {
+            userEvent.click(button)
+            expect(mock).toHaveBeenCalledTimes(index + 1)
+            if (button.textContent === 'Previous')
+                expect(mock).toHaveBeenLastCalledWith(current - 1)
+            else if (button.textContent === 'Next')
+                expect(mock).toHaveBeenLastCalledWith(current + 1)
+            else expect(mock).toHaveBeenLastCalledWith(parseInt(button.textContent ?? '-1'))
+        }
+    }
+)
