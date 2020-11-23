785afbbd0fa3143cbb729a7898843b48b2fb4362
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-11-23 15:51:25 -0800

    Updated <Progress>

--- elements/progress.tsx
+++ elements/progress.tsx
@@ -6,23 +6,36 @@ type Properties = {
     value: number
     max?: number
     color?: Colors
+    size?: 'default' | 'large'
 }
 
 const COLORS_MAP: Record<Colors, string> = {
     primary: 'bg-primary dark:bg-primary-d',
-    secondary: 'bg-gray-600 dark:bg-gray-d600',
+    secondary: 'bg-gray dark:bg-gray-d',
     success: 'bg-success dark:bg-success-d',
     error: 'bg-error dar:bg-error-d',
     warning: 'bg-warning dark:bg-warning-d',
 }
 
-export const Progress = ({ value, max = 100, color = 'secondary' }: Properties): JSX.Element => {
+export const Progress = ({
+    value,
+    max = 100,
+    color = 'secondary',
+    size = 'default',
+}: Properties): JSX.Element => {
+    const containerClasses = classNames(
+        'relative w-full bg-gray-50 rounded-full dark:bg-gray-d50',
+        {
+            'h-2': size === 'default',
+            'h-4': size === 'large',
+        }
+    )
     const barClasses = classNames(
         'h-full transition-all duration-150 rounded-full',
         COLORS_MAP[color]
     )
     return (
-        <div className='relative w-full h-2 bg-gray-200 rounded-full dark:bg-gray-d200'>
+        <div className={containerClasses}>
             <div className={barClasses} style={{ width: `${(value / max) * 100}%` }} />
         </div>
     )
