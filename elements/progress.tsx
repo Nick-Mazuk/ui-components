2a513f9a7324b3735eccc1468d1f422bafaf370d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-11-23 16:31:20 -0800

    Updated Progress styling

--- elements/progress.tsx
+++ elements/progress.tsx
@@ -1,12 +1,13 @@
 import classNames from 'classnames'
 
-type Colors = 'primary' | 'secondary' | 'success' | 'error' | 'warning'
+type Colors = 'primary' | 'secondary' | 'success' | 'error' | 'warning' | 'dark'
 
 type Properties = {
     value: number
     max?: number
     color?: Colors
     size?: 'default' | 'large'
+    transparentBackground?: boolean
 }
 
 const COLORS_MAP: Record<Colors, string> = {
@@ -15,6 +16,7 @@ const COLORS_MAP: Record<Colors, string> = {
     success: 'bg-success dark:bg-success-d',
     error: 'bg-error dar:bg-error-d',
     warning: 'bg-warning dark:bg-warning-d',
+    dark: 'bg-gray-700 dark:bg-gray-d700',
 }
 
 export const Progress = ({
@@ -22,14 +24,13 @@ export const Progress = ({
     max = 100,
     color = 'secondary',
     size = 'default',
+    transparentBackground = false,
 }: Properties): JSX.Element => {
-    const containerClasses = classNames(
-        'relative w-full bg-gray-50 rounded-full dark:bg-gray-d50',
-        {
-            'h-2': size === 'default',
-            'h-4': size === 'large',
-        }
-    )
+    const containerClasses = classNames('relative w-full rounded-full dark:bg-gray-d50', {
+        'h-2': size === 'default',
+        'h-4': size === 'large',
+        'bg-gray-50': !transparentBackground,
+    })
     const barClasses = classNames(
         'h-full transition-all duration-150 rounded-full',
         COLORS_MAP[color]
