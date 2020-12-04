0c6491d4171338185a0de92a03827296b33200f9
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-12-04 14:33:39 -0800

    Fixed navbar brand size

--- layouts/navbar/helpers/brand.tsx
+++ layouts/navbar/helpers/brand.tsx
@@ -1,3 +1,4 @@
+import { useNavbarContext } from '..'
 import { Link } from '../../../elements/link'
 import { Text } from '../../../elements/text'
 
@@ -7,9 +8,10 @@ export type BrandProps = {
 }
 
 export const Brand = ({ content, href = '/' }: BrandProps): JSX.Element => {
+    const { size } = useNavbarContext()
     return (
         <Link href={href} className='flex items-center self-stretch select-none'>
-            <Text h5 as='p'>
+            <Text h5={size === 'default'} h6={size === 'small'} as='p'>
                 {content}
             </Text>
         </Link>
