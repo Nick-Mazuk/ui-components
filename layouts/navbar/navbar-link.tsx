c2e6e3670db909a1a50ee91ff835f36feeb7d71d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-12-04 16:21:51 -0800

    Fixed navbar link colors

--- layouts/navbar/navbar-link.tsx
+++ layouts/navbar/navbar-link.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router'
 import { Link } from '../../elements/link'
 import type { TextChildren } from '../../elements/text'
 import { Text } from '../../elements/text'
+import type { Color } from '../header'
+import { useHeaderContext } from '../header'
 import type { Breakpoint } from './navbar-item-wrapper'
 import { NavbarItemWrapper } from './navbar-item-wrapper'
 
@@ -14,21 +16,47 @@ type Props = {
     breakpoint?: Breakpoint
 }
 
+type ElementColors = {
+    text: string
+    activeDiv: string
+}
+
+const COLOR_MAP: Record<Color, ElementColors> = {
+    white: {
+        text: 'text-gray-600 dark:text-gray-d600 hover:text-primary dark:hover:text-primary-d',
+        activeDiv: 'bg-primary',
+    },
+    dark: {
+        text: 'hover:text-white dark:hover:text-black',
+        activeDiv: 'bg-primary-d400',
+    },
+    primary: {
+        text: 'text-primary-100 hover:text-white dark:text-primary-d100 dark:hover:text-black',
+        activeDiv: 'bg-primary-50',
+    },
+}
+
 export const NavbarLink = ({ content, href, breakpoint }: Props): JSX.Element => {
     const router = useRouter()
     const isCurrentRoute = router.pathname.includes(href)
-    const classes = 'relative flex items-center self-stretch select-none last:-mr-2'
-    const borderClasses = classNames('h-1 bg-primary absolute left-0 w-full bottom-0', {
-        'opacity-100': isCurrentRoute,
-        'opacity-0': !isCurrentRoute,
-    })
+    const { color } = useHeaderContext()
+    const classes = classNames(
+        'relative flex items-center self-stretch select-none last:-mr-2',
+        COLOR_MAP[color].text
+    )
+    const borderClasses = classNames(
+        'h-1 absolute left-0 w-full bottom-0',
+        COLOR_MAP[color].activeDiv,
+        {
+            'opacity-100': isCurrentRoute,
+            'opacity-0': !isCurrentRoute,
+        }
+    )
     return (
         <NavbarItemWrapper breakpoint={breakpoint}>
             <Link href={href} className={classes}>
                 <div className='relative px-3'>
-                    <Text color='text-gray-600 dark:text-gray-d600 hover:text-primary dark:hover:text-primary-d'>
-                        {content}
-                    </Text>
+                    <Text>{content}</Text>
                 </div>
                 <div className={borderClasses} />
             </Link>
