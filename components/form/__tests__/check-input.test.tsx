846d138579e7bc6a205a69ebc197f3a4560ea3e7
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-12-17 20:06:50 -0800

    Checkbox input added

---
+++ components/form/__tests__/check-input.test.tsx
@@ -0,0 +1,255 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import type { FormSync } from '..'
+import { Form } from '..'
+import { Button } from '../../../elements/button'
+import { CheckBoxInput } from '../check-box-input'
+
+test('Check input renders without crashing', () => {
+    const { baseElement } = render(<CheckBoxInput name='check' label='Some label text' />)
+    expect(baseElement).not.toBeEmptyDOMElement()
+})
+
+test('Can set the id of the checkbox', () => {
+    render(<CheckBoxInput name='check' label='Some label text' />)
+    expect(screen.getByRole('checkbox').getAttribute('id')).toBe('check')
+})
+
+test('Make sure the label text is present', () => {
+    render(<CheckBoxInput name='check' label='Some label text' />)
+    expect(screen.getByText('Some label text')).toBeTruthy()
+})
+
+test('Checkbox is unchecked by default', () => {
+    render(<CheckBoxInput name='check' label='Some label text' />)
+    expect(screen.getByRole('checkbox')).not.toBeChecked()
+})
+
+test('Default value of "checked" works', () => {
+    render(<CheckBoxInput name='check' label='Some label text' defaultValue='checked' />)
+    expect(screen.getByRole('checkbox')).toBeChecked()
+})
+
+test('Default value of "unchecked" works', () => {
+    render(<CheckBoxInput name='check' label='Some label text' defaultValue='unchecked' />)
+    expect(screen.getByRole('checkbox')).not.toBeChecked()
+})
+
+test('Clicking on the label updates check state', () => {
+    render(<CheckBoxInput name='check' label='Some label text' />)
+    const checkBox = screen.getByRole('checkbox')
+    const label = screen.getByLabelText('Some label text')
+    userEvent.click(label)
+    expect(checkBox).toBeChecked()
+    userEvent.click(label)
+    expect(checkBox).not.toBeChecked()
+    userEvent.click(label)
+    expect(checkBox).toBeChecked()
+    userEvent.click(label)
+    expect(checkBox).not.toBeChecked()
+})
+
+test("If it's disabled, clicking on the label does nothing", () => {
+    render(<CheckBoxInput name='check' label='Some label text' disabled />)
+    const checkBox = screen.getByRole('checkbox')
+    const label = screen.getByLabelText('Some label text')
+    userEvent.click(label)
+    expect(checkBox).not.toBeChecked()
+    userEvent.click(label)
+    expect(checkBox).not.toBeChecked()
+})
+
+test("If optional, it's labeled as optional", () => {
+    render(<CheckBoxInput name='check' label='Some label text' optional />)
+    expect(screen.getByText(/optional/iu)).toBeTruthy()
+})
+
+test('If required, optional label is not shown', () => {
+    render(<CheckBoxInput name='check' label='Some label text' />)
+    expect(screen.queryByText(/optional/iu)).toBeFalsy()
+})
+
+test('If required, checkbox is marked as invalid when unchecked', () => {
+    render(<CheckBoxInput name='check' label='Some label text' />)
+    const checkBox = screen.getByRole('checkbox')
+    const label = screen.getByLabelText('Some label text')
+    expect(checkBox.getAttribute('aria-invalid')).toBe('false')
+    userEvent.click(label)
+    expect(checkBox.getAttribute('aria-invalid')).toBe('false')
+    userEvent.click(label)
+    expect(checkBox.getAttribute('aria-invalid')).toBe('true')
+})
+
+// eslint-disable-next-line max-lines-per-function -- multiple short tests
+describe('Syncs with formSync', () => {
+    test('Syncs with form initially', () => {
+        const formSync: FormSync = {
+            state: 'ready',
+            updateForm: jest.fn(),
+            data: {},
+        }
+        render(<CheckBoxInput name='check' label='Some label text' formSync={formSync} />)
+        expect(formSync.updateForm).toHaveBeenCalledTimes(1)
+        expect(formSync.updateForm).toHaveBeenCalledWith(
+            'check',
+            false,
+            expect.any(Function),
+            expect.any(Function)
+        )
+
+        const label = screen.getByLabelText('Some label text')
+        userEvent.click(label)
+
+        expect(formSync.updateForm).toHaveBeenCalledTimes(2)
+        expect(formSync.updateForm).toHaveBeenCalledWith(
+            'check',
+            true,
+            expect.any(Function),
+            expect.any(Function)
+        )
+    })
+
+    test('Syncs with default value', () => {
+        const formSync: FormSync = {
+            state: 'ready',
+            updateForm: jest.fn(),
+            data: {},
+        }
+        render(
+            <CheckBoxInput
+                name='check'
+                label='Some label text'
+                defaultValue='checked'
+                formSync={formSync}
+            />
+        )
+        expect(formSync.updateForm).toHaveBeenCalledTimes(1)
+        expect(formSync.updateForm).toHaveBeenCalledWith(
+            'check',
+            true,
+            expect.any(Function),
+            expect.any(Function)
+        )
+    })
+
+    test('When form is submitted, it returns to its default value', async () => {
+        const mockSubmit = jest.fn(() => Promise.resolve(true))
+        const onSuccessMock = jest.fn()
+        render(
+            <Form handleSubmit={mockSubmit} onSuccess={onSuccessMock} clearOnSubmit>
+                {(formSync) => {
+                    return (
+                        <>
+                            <CheckBoxInput
+                                name='check'
+                                label='Some label text'
+                                formSync={formSync}
+                            />
+                            <Button value='Submit' color='primary' type='submit' />
+                        </>
+                    )
+                }}
+            </Form>
+        )
+
+        const label = screen.getByLabelText('Some label text')
+        const checkBox = screen.getByRole('checkbox')
+        userEvent.click(label)
+        expect(checkBox).toBeChecked()
+        userEvent.click(screen.getByRole('button'))
+        await waitFor(() => expect(mockSubmit).toHaveBeenCalled())
+        expect(checkBox).not.toBeChecked()
+    })
+
+    test('If optional, it allows the form to submit when off', () => {
+        const mockSubmit = jest.fn(() => Promise.resolve(true))
+        render(
+            <Form handleSubmit={mockSubmit}>
+                {(formSync) => {
+                    return (
+                        <>
+                            <CheckBoxInput
+                                name='check'
+                                label='Some label text'
+                                formSync={formSync}
+                                optional
+                            />
+                            <Button value='Submit' color='primary' type='submit' />
+                        </>
+                    )
+                }}
+            </Form>
+        )
+
+        userEvent.click(screen.getByRole('button'))
+        expect(mockSubmit).toHaveBeenCalled()
+    })
+
+    test('If optional, it allows the form to submit when on', () => {
+        const mockSubmit = jest.fn(() => Promise.resolve(true))
+        render(
+            <Form handleSubmit={mockSubmit}>
+                {(formSync) => {
+                    return (
+                        <>
+                            <CheckBoxInput
+                                name='check'
+                                label='label'
+                                formSync={formSync}
+                                defaultValue='checked'
+                                optional
+                            />
+                            <Button value='Submit' color='primary' type='submit' />
+                        </>
+                    )
+                }}
+            </Form>
+        )
+
+        userEvent.click(screen.getByRole('button'))
+        expect(mockSubmit).toHaveBeenCalled()
+    })
+
+    test('If required, it allows throws error when off', () => {
+        const mockSubmit = jest.fn(() => Promise.resolve(true))
+        render(
+            <Form handleSubmit={mockSubmit}>
+                {(formSync) => {
+                    return (
+                        <>
+                            <CheckBoxInput name='check' label='label' formSync={formSync} />
+                            <Button value='Submit' color='primary' type='submit' />
+                        </>
+                    )
+                }}
+            </Form>
+        )
+
+        userEvent.click(screen.getByRole('button'))
+        expect(mockSubmit).not.toHaveBeenCalled()
+    })
+    test('If required, it allows the form to submit when on', () => {
+        const mockSubmit = jest.fn(() => Promise.resolve(true))
+        render(
+            <Form handleSubmit={mockSubmit}>
+                {(formSync) => {
+                    return (
+                        <>
+                            <CheckBoxInput
+                                name='check'
+                                label='label'
+                                defaultValue='checked'
+                                formSync={formSync}
+                            />
+                            <Button value='Submit' color='primary' type='submit' />
+                        </>
+                    )
+                }}
+            </Form>
+        )
+
+        userEvent.click(screen.getByRole('button'))
+        expect(mockSubmit).toHaveBeenCalled()
+    })
+})
---
+++ components/form/check-box-input.tsx
@@ -0,0 +1,110 @@
+import type { ChangeEvent } from 'react'
+import { useEffect, useState } from 'react'
+
+import classNames from 'classnames'
+
+import type { FormSync } from '.'
+import { CheckSquare, Square } from '../../elements/icon'
+import { Text } from '../../elements/text'
+
+type Props = {
+    name: string
+    label: string
+    defaultValue?: 'checked' | 'unchecked'
+    formSync?: FormSync
+    optional?: boolean
+    disabled?: boolean
+}
+
+// eslint-disable-next-line max-lines-per-function -- WILL FIX
+export const CheckBoxInput = ({
+    name,
+    label,
+    defaultValue = 'unchecked',
+    optional = false,
+    disabled = false,
+    formSync,
+}: Props): JSX.Element => {
+    const [checked, setChecked] = useState(defaultValue === 'checked')
+    const [isValid, setIsValid] = useState(true)
+    const labelClasses = classNames('block cursor-pointer relative py-1', {
+        'text-gray-300': disabled,
+    })
+    const checkedClasses = classNames('absolute top-0 w-full', { 'opacity-0': !checked })
+    const uncheckedClasses = classNames('absolute top-0 w-full', { 'opacity-0': checked })
+    const invalidClasses = classNames('absolute border rounded -inset-x-2 inset-y-0 border-error', {
+        'opacity-0': isValid,
+    })
+    let hasBeenClicked = false
+
+    const validate = (newCheckState: boolean): boolean => {
+        if (newCheckState || optional || disabled) return true
+        return false
+    }
+
+    const resetToDefault = () => {
+        setChecked(defaultValue === 'checked')
+        hasBeenClicked = false
+    }
+
+    const updateFormValidation = () =>
+        isValid && !(!hasBeenClicked && !optional && defaultValue !== 'checked')
+
+    const syncWithForm = (newCheckState: boolean) => {
+        if (formSync) formSync.updateForm(name, newCheckState, updateFormValidation, resetToDefault)
+    }
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const newCheckState = event.target.checked
+        setChecked(newCheckState)
+        setIsValid(validate(newCheckState))
+        syncWithForm(newCheckState)
+        hasBeenClicked = true
+    }
+
+    useEffect(() => {
+        syncWithForm(checked)
+        // eslint-disable-next-line react-hooks/exhaustive-deps -- should only run once, otherwise infinite loops
+    }, [])
+
+    return (
+        <label htmlFor={name} className={labelClasses}>
+            <div className={invalidClasses} />
+            <div className='flex space-x-2 flex-start'>
+                <div className='flex-none w-4 h-4 mt-1'>
+                    <div aria-hidden className='relative'>
+                        <div className={checkedClasses}>
+                            <CheckSquare />
+                        </div>
+                        <div className={uncheckedClasses}>
+                            <Square />
+                        </div>
+                    </div>
+                    <input
+                        className='sr-only'
+                        id={name}
+                        name={name}
+                        type='checkbox'
+                        checked={checked}
+                        onChange={handleChange}
+                        required={!optional}
+                        aria-invalid={!isValid}
+                        disabled={disabled}
+                    />
+                </div>
+                <Text small>
+                    {label}
+                    {optional ? (
+                        <Text span color='text-gray'>
+                            (optional)
+                        </Text>
+                    ) : (
+                        ''
+                    )}
+                </Text>
+            </div>
+        </label>
+    )
+}
+
+export type CheckBoxInput = ReturnType<typeof CheckBoxInput>
