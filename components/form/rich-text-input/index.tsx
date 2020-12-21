fb09becd393d727022b5fd6cc3d561d908b37d17
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-12-20 17:17:29 -0800

    Fixed label bug

--- components/form/rich-text-input/index.tsx
+++ components/form/rich-text-input/index.tsx
@@ -186,7 +186,7 @@ const getMaxCharacterProgress = (html: string, maxCharacters: number | undefined
 
 // eslint-disable-next-line max-lines-per-function, sonarjs/cognitive-complexity -- it's going to be long
 export const RichTextInput = (props: Props): JSX.Element => {
-    const [label, name] = getIdentificationData(props)
+    const [name, label] = getIdentificationData(props)
     const [value, setValue] = useState(props.defaultValue ?? '<p></p>')
     const [isValid, setIsValid] = useState(true)
 
