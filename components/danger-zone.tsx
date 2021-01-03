ca82da7432fad4fc2282711d52a9b0de717f312d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-01-02 20:38:34 -0800

    Fixed danger zone styling error -- button is now glued to the right

--- components/danger-zone.tsx
+++ components/danger-zone.tsx
@@ -24,6 +24,7 @@ export const EmptyState = ({ title, description, buttonText, onClick }: Props): 
                         size='small'
                         style='text'
                         onClick={onClick}
+                        glue={['right']}
                     />
                 </div>
             </div>
