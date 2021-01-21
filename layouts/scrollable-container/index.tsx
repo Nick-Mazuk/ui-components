ca36097da9a85ff4c95fa1b04ad134c7002d9acb
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-01-21 12:58:11 -0800

    Revert scrollable container indexing

--- layouts/scrollable-container/index.tsx
+++ layouts/scrollable-container/index.tsx
@@ -159,8 +159,8 @@ Props): JSX.Element => {
                 }}
                 ref={containerRef}
             >
-                {childrenArray.map((child, index) => (
-                    <div key={index} style={{ scrollSnapAlign: 'start' }}>
+                {childrenArray.map((child) => (
+                    <div key={child?.toString()} style={{ scrollSnapAlign: 'start' }}>
                         {child}
                     </div>
                 ))}
