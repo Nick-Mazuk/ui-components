a45cab1eba4fbf73283f5ef305ff1083aa78b236
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-01-27 08:21:57 -0800

    Add fullWidth option to navbar dropdown

--- layouts/navbar/dropdown/index.tsx
+++ layouts/navbar/dropdown/index.tsx
@@ -15,6 +15,7 @@ type Props = {
     item: ReactNode
     width?: string
     align?: 'left' | 'right'
+    fullWidth?: boolean
 }
 
 const COLOR_MAP: Record<Color, string> = {
@@ -28,21 +29,24 @@ export const NavbarDropdown = ({
     item,
     width = 'w-64',
     align = 'left',
+    fullWidth = false,
 }: Props): JSX.Element => {
     const { size } = useNavbarContext()
     const { color } = useHeaderContext()
+    const containerClasses = classNames('group self-stretch flex', { relative: !fullWidth })
     const dropdownClasses = classNames(
         'absolute group-hover:block overflow-hidden hidden shadow-md rounded-b-lg border-t-0',
-        width,
+        fullWidth ? '' : width,
         COLOR_MAP[color],
         {
-            'right-0': align === 'right',
+            'right-0': align === 'right' && !fullWidth,
             'top-12': size === 'small',
             'top-16': size === 'default',
+            'left-0 right-0': fullWidth,
         }
     )
     return (
-        <div className='relative group self-stretch flex'>
+        <div className={containerClasses}>
             <div className={dropdownClasses}>{children}</div>
             <div className='flex items-center cursor-pointer pl-3'>{item}</div>
         </div>
