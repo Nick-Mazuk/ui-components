bb63ac88f4060eed57087737b280fcd43c1f1e9d
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-01-28 09:19:00 -0800

    Add two new icons -- PlayCircle and PauseCircle

--- elements/icon.tsx
+++ elements/icon.tsx
@@ -795,6 +795,14 @@ export const Music = (props: IconProps): JSX.Element => {
     )
 }
 
+export const PauseCircle = (props: IconProps): JSX.Element => {
+    return icon('M10 15V9 M14 15V9 M2,12a10,10 0 1,0 20,0a10,10 0 1,0 -20,0', {
+        stroke: true,
+        width: props.width,
+        name: 'PauseCircle',
+    })
+}
+
 export const Phone = (props: FillableIconProps): JSX.Element => {
     return icon(
         'M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z',
@@ -805,6 +813,14 @@ export const Phone = (props: FillableIconProps): JSX.Element => {
             name: 'Phone',
         }
     )
+}
+
+export const PlayCircle = (props: IconProps): JSX.Element => {
+    return icon('M10 8l6 4-6 4V8z M2,12a10,10 0 1,0 20,0a10,10 0 1,0 -20,0', {
+        stroke: true,
+        width: props.width,
+        name: 'PlayCircle',
+    })
 }
 
 export const Plus = (props: IconProps): JSX.Element => {
