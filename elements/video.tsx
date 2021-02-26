03f76bcef78101c161e2e76b7cf9ec8de7a4ea52
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-02-25 16:13:44 -0800

    Allow YT video tracking by Google Analytics

--- elements/video.tsx
+++ elements/video.tsx
@@ -26,7 +26,7 @@ type YouTubeProperties = {
 export const YouTube = ({ id, title, noRatio }: YouTubeProperties): JSX.Element => {
     const indexFrame = (
         <iframe
-            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+            src={`https://www.youtube.com/embed/${id}?autoplay=1?enablejsapi=1`}
             srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${id}?autoplay=1><img src='https://i.ytimg.com/vi/${id}/maxresdefault.jpg' onload="if (this.naturalWidth === 120) {this.src = this.currentSrc.replace('maxresdefault', 'hqdefault');}" alt='${title}'><span>▶</span></a>`}
             frameBorder='0'
             allow='autoplay; encrypted-media; picture-in-picture'
