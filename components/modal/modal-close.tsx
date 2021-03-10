4fd37ab73bb3f8d9806f2213a28143e28c35dc51
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-03-10 12:08:26 -0800

    Update modal close position

--- components/modal/modal-close.tsx
+++ components/modal/modal-close.tsx
@@ -11,7 +11,7 @@ export const ModalClose = ({ onClick }: Props): JSX.Element => {
     return (
         <WithClick
             onClick={onClick}
-            className='h-12 w-12 flex items-center justify-center -mx-8 -my-6 transition-colors duration-150 text-gray hover:text-gray-900 hover:bg-gray-30 focus:text-gray-900 focus:bg-gray-30 focus:outline-none'
+            className='h-12 w-12 flex items-center justify-center absolute top-2 right-2 rounded-full transition-colors duration-150 text-gray hover:text-gray-900 hover:bg-gray-30 focus:text-gray-900 focus:bg-gray-30 focus:outline-none'
             name='Close modal'
         >
             <X width='w-5' />
