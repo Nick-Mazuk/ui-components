9599deef071914feed32ddd9b4ae51a818625404
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-03-10 12:16:30 -0800

    Update modal stylings for alignment

--- components/modal/modal-actions.tsx
+++ components/modal/modal-actions.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 export const ModalActions = ({ children }: Props): JSX.Element => {
-    return <div className='pl-8 pr-4 pb-4 flex justify-end space-x-2'>{children}</div>
+    return <div className='px-8 pb-6 flex justify-end space-x-2'>{children}</div>
 }
 
 export type ModalActions = ReturnType<typeof ModalActions>
